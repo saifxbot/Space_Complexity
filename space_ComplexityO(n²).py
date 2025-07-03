86e0887e35b4b0cc673f5c5585be121697639600
Saif Ibna Reza <saifibnareza078@gmail.com>
2025-07-03 10:39:04 +0300

    fixed comments

--- space_ComplexityO(n²).py
+++ space_ComplexityO(n²).py
@@ -3,7 +3,7 @@ def print_all_pairs(arr):
         for j in arr:
             print(i, j) # This function prints all pairs of elements in the array.
                         # The space complexity is O(n^2) 
-                        #because we are generating pairs for each element
+                        # because we are generating pairs for each element
 
 data = [1, 2, 3]
 print_all_pairs(data)
