6abe0d3c46e20fd1ea68ded8c5da22b883ed512b
Saif Ibna Reza <saifibnareza078@gmail.com>
2025-07-03 10:27:48 +0300

    Space_Complexity_Example

---
+++ sapce_ComplexityO(1).py
@@ -0,0 +1,8 @@
+arr = [1, 2, 3, 4, 5]
+
+def print_first_item(arr):
+    print(arr[0])
+
+print_first_item(arr)
+# Space Complexity: O(1)
+# This function uses a constant amount of space regardless of the input size.  
